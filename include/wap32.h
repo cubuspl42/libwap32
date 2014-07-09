8b2ac85b41c32e3bb72bfe80a9fbc54b622d182b
cubuspl42 <kuba@jakubs-air.lan>
2014-07-09 20:42:05 +0200

    New header errors.h

--- include/wap32.h
+++ include/wap32.h
@@ -2,5 +2,7 @@
 #define wap32_h
 
 #include "wap32/common.h"
+#include "wap32/errors.h"
+#include "wap32/wwd.h"
 
 #endif
---
+++ include/wap32/errors.h
@@ -0,0 +1,11 @@
+#ifndef wap32_errors_h
+#define wap32_errors_h
+
+#include "common.h"
+
+enum {
+    WAP32_OK = 0,
+    WAP32_ERROR = -1,
+};
+
+#endif
