419c89483b1650ffdfb82e071649bcdbfecc6388
cubuspl42 <kuba@jakubs-air.lan>
2014-07-10 21:09:02 +0200

    InputStream now throws EndOfBuffer exception

--- src/io.h
+++ src/io.h
@@ -3,38 +3,43 @@
 
 #include "common.h"
 #include <cassert>
+#include <exception>
 #include <string.h>
 #include <utility>
 
 class InputStream
 {
 public:
+    struct EndOfBuffer : std::exception {};
+    
     inline InputStream(const char *buffer, size_t buffer_size)
     : m_buffer(buffer)
-    , m_offset(0)
-    , m_size(buffer_size) {}
+    , m_size(buffer_size)
+    , m_offset(0) {}
+    template<class Containter> InputStream(const Containter &c)
+    : m_buffer(c.data())
+    , m_size(c.size())
+    , m_offset(0) {}
     inline void set_offset(unsigned offset) {
         m_offset = offset;
     }
     template<typename T, typename... Tail>
     void read(T&& val, Tail&&... tail) {
         read_impl(std::forward<T>(val));
-        return read(std::forward<Tail>(tail)...);
+        read(std::forward<Tail>(tail)...);
     }
 private:
     inline void read() {}
     template<typename T>
     void read_impl(T &val) {
         if(m_size - m_offset < sizeof(T))
-            wap32_err__throw(WAP32_EINVALIDDATA, "Unexpected end of buffer");
+            throw EndOfBuffer();
         memcpy(&val, m_buffer + m_offset, sizeof(T));
         m_offset += sizeof(T);
     }
     template<typename T>
     void read_impl(const T &constant) {
         T tmp {};
-        if(tmp != constant)
-            wap32_err__warning("Value doesn't equal expected constant");
         read_impl(tmp);
     }
     template<typename T, size_t size>
@@ -44,8 +49,8 @@ private:
     }
     
     const char *m_buffer;
-    unsigned m_offset;
     size_t m_size;
+    unsigned m_offset;
 };
 
 #endif
