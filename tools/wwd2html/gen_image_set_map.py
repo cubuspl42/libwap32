22ff96bb3b9bc3b9fe804d3ac67eb0cccc3707fb
cubuspl42 <cubuspl42@tesign.pl>
2014-08-10 18:40:17 +0200

    New image_set_map.dict format

--- tools/wwd2html/gen_image_set_map.py
+++ tools/wwd2html/gen_image_set_map.py
@@ -1,18 +1,25 @@
-from pprint import pprint
 import os
 from os import path
+from pprint import pprint
+import struct
+import sys
 
 image_set_map = {}
-rez = 'CLAW'
-for root, dirs, files in os.walk(rez):
-    for ignored in ('INTERFACE', 'STATES'):
-        if ignored in dirs:
-            dirs.remove(ignored)
-    if not len(dirs) and len(files):
-        key = root.replace('/', '_').replace(rez + '_', '')
-        value = path.join(root, files[0])
-        print(key, value)
-        image_set_map[key] = value
+unpacked_rez_path = sys.argv[1]
+for root, dirs, files in os.walk(unpacked_rez_path):
+    if not dirs and files:
+        first_file = files[0]
+        if first_file.endswith(".PID"):
+            offset = None
+            file_path = path.join(root, first_file)
+            with open(file_path, 'rb') as pid_file:
+                pid_file.read(16)
+                offset = struct.unpack("<ii", pid_file.read(8))
+            key = path.relpath(root, unpacked_rez_path).replace(os.sep, '_')
+            rel_file_path = path.relpath(file_path, unpacked_rez_path)
+            value = (rel_file_path.replace(os.sep, '/').replace('.PID', ''), offset)
+            print(key, value)
+            image_set_map[key] = value
 
 file = open('image_set_map.dict', 'w')
 pprint(image_set_map, file)
