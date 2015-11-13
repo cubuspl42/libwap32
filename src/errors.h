101e9e428553380a851ebd23109be2df0d4bc2f8
cubuspl42 <cubuspl42@stgn.pl>
2015-11-13 16:30:47 +0100

    clang-format headers too

--- src/errors.h
+++ src/errors.h
@@ -16,26 +16,27 @@
 #endif
 
 namespace wap {
-    class Error {
-    public:
-        inline Error(int error_code) : error_code_(error_code) {}
-        inline int error_code() { return error_code_; }
-        static Error from_zlib_error(int zlib_error_code);
-    private:
-        int error_code_;
-    };
-    
-    template<class F, typename... Args>
-    int handle_exceptions(F f, Args&&... args) {
-        try {
-            f(std::forward<Args>(args)...);
-        } catch(wap::Error &error) {
-            return error.error_code();
-        } catch(std::bad_alloc&) {
-            return WAP_ENOMEMORY;
-        }
-        return WAP_OK;
+class Error {
+  public:
+    inline Error(int error_code) : error_code_(error_code) {}
+    inline int error_code() { return error_code_; }
+    static Error from_zlib_error(int zlib_error_code);
+
+  private:
+    int error_code_;
+};
+
+template <class F, typename... Args>
+int handle_exceptions(F f, Args &&... args) {
+    try {
+        f(std::forward<Args>(args)...);
+    } catch (wap::Error &error) {
+        return error.error_code();
+    } catch (std::bad_alloc &) {
+        return WAP_ENOMEMORY;
     }
+    return WAP_OK;
+}
 }
 
 #endif
--- src/wwd.h
+++ src/wwd.h
@@ -20,14 +20,14 @@ struct wap_plane {
     uint32_t tiles_high = 0;
     wap_plane_properties properties;
     std::vector<uint32_t> tiles;
-	std::vector<std::string> image_sets;
+    std::vector<std::string> image_sets;
     std::vector<wap_object> objects;
 };
 
 struct wap_wwd {
     uint32_t checksum = 0;
     wap_wwd_properties properties;
-	std::vector<wap_plane> planes;
+    std::vector<wap_plane> planes;
     std::vector<wap_tile_description> tile_descriptions;
 };
 
